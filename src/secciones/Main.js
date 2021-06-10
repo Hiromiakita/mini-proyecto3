cffffd9c2c4db745ff8ce17a9dc303bfca63e13b
hiromiakita <hiromiakitao@gmail.com>
2021-06-09 21:18:44 -0500

    crear función delete

--- src/secciones/Main.js
+++ src/secciones/Main.js
@@ -24,6 +24,20 @@ const Main = () => {
             });
     }
 
+
+    // Función que elimina una tarea
+    const deleteTarea = (id) => {
+        const URL = `https://mini-proyecto3-m5-default-rtdb.firebaseio.com/tareas/${id}.json`;
+
+        fetch(URL, { method: 'DELETE' })
+            .then(body => body.json)
+            .then(respuesta => console.log(respuesta));
+    }
+
+    // 1. Enviar como prop la función deleteTarea al componente TareaCard
+    // 2. En TareaCard necesitamos recibir como prop la función deleteTarea
+    // 3. Mandar llamar función en TareaCard cuando se haga click en el botón eliminar
+
     // El callback del useEffect se ejecutará antes de que el componente se monte
     useEffect(() => {
         getTareas();
@@ -31,18 +45,27 @@ const Main = () => {
 
 
     return (
-        <div>
+        <div className="container">
             <h1>Soy la sección principal</h1>
-            <TareaCard 
+            {/* <TareaCard 
                 titulo="Titulo X" 
                 descripcion="Descripción X" 
                 completada={false}
-            />
-
+            /> */}
             {/* ERROR */}
             {/* {
                 tareas.map(tarea => <TareaCard/>)
             } */}
+            <div className="d-flex flex-wrap">
+                {
+                    Object.keys(tareas).map(identificador => 
+                        <TareaCard 
+                            titulo = {tareas[identificador].titulo} 
+                            descripcion = {tareas[identificador].descripcion}
+                            completada = {tareas[identificador].completada}
+                            key={identificador}/>)
+                }
+            </div>
         </div>
     )
 }
