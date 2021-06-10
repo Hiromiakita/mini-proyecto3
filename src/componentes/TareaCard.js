7189a030bb250c4876b0ba4cf7fae0fe734957d3
hiromiakita <hiromiakitao@gmail.com>
2021-06-09 21:18:30 -0500

    añadir botón eliminar

--- src/componentes/TareaCard.js
+++ src/componentes/TareaCard.js
@@ -5,13 +5,16 @@ const TareaCard = (props) => {
     // Destructurar: Sacar las propiedades del objeto y guardarlas en variables
     const {titulo, descripcion, completada} = props;
 
-    console.log('props', props);
     return (
         <div className="card" style={{width: "18rem"}}>
             <div className="card-body">
                 <h5 className="card-title">{titulo}</h5>
                 <p className="card-text">{descripcion}</p>
-                <a href="a" className="btn btn-primary">Completar</a>
+                <div className="d-flex justify-content-between">
+                    <a href="a" className="btn btn-primary">Completar</a>
+                    <a href="a" className="btn btn-danger">Eliminar</a>
+                </div>
+                
             </div>
         </div>
     )
