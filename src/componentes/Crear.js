4edac6d1bb2229b150e72f4d2307b619f978a9f1
hiromiakita <hiromiakitao@gmail.com>
2021-06-10 20:12:26 -0500

    componente crear

---
+++ src/componentes/Crear.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Crear = () => {
+    return (
+        <form>
+            <div className="mb-3">
+                <label for="inputTitulo" className="form-label">Titulo Tarea</label>
+                <input type="text" className="form-control" id="inputTitulo" aria-describedby="titulo"/>
+            </div>
+            <div className="mb-3">
+                <label for="inputDescripcion" className="form-label">Descripción Tarea</label>
+                <input type="text" className="form-control" id="inputDescripcion" />
+            </div>
+            <button type="submit" className="btn btn-primary">Crear Tarea</button>
+        </form>
+    )
+}
+
+export default Crear
